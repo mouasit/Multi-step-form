d9446d1b5b0cc3b189f042711fd45a4cc30a876f
mouasit <mustaphaouassit60@gmail.com>
2023-05-30 15:59:40 +0100

    add : heading

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Form() {
-  return <div></div>;
+  return (
+    <div className="text-MarineBlue">
+      <div>
+        <h1 className="font-bold text-[1.9rem]">Personal info</h1>
+      </div>
+    </div>
+  );
 }
