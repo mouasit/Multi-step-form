3f5a4f8787ef75a0430e43d75cfbe232a2e1fd46
mouasit <mustaphaouassit60@gmail.com>
2023-06-12 16:30:10 +0100

    add: select plan info

--- src/Components/CardPlan.js
+++ src/Components/CardPlan.js
@@ -1,17 +1,29 @@
 import React from "react";
 
-export default function CardPlan({ icon, type, price, active }) {
+export default function CardPlan({
+  icon,
+  type,
+  price,
+  index,
+  active,
+  setActive,
+  offer,
+}) {
   return (
     <button
       type="button"
-      className={`flex flex-1 items-center gap-4 rounded-lg border-[1px] p-5 lg:h-[11.5rem] lg:flex-col lg:items-start lg:justify-between ${
-        active ? "border-MarineBlue bg-Alabaster" : "border-LightGray"
+      className={`flex flex-1 items-center gap-4 rounded-lg border-[1px] p-5 hover:border-MarineBlue lg:h-[11.5rem] lg:flex-col lg:items-start lg:justify-between ${
+        active === index ? "border-MarineBlue bg-Alabaster" : "border-LightGray"
       }`}
+      onClick={() => {
+        setActive(index);
+      }}
     >
       {icon}
       <span className="flex flex-col items-start">
         <span className=" text-[1.1rem] font-bold capitalize">{type}</span>
         <span className="text-CoolGray">{price}</span>
+        {offer ? <span className="text-MarineBlue">{offer}</span> : null}
       </span>
     </button>
   );
--- src/Components/SelectPlan.js
+++ src/Components/SelectPlan.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CardPlan from "./CardPlan";
 import { IconArcade, IconAdvanced, IconPro } from "./Icons";
 
 export default function SelectPlan() {
+  const [active, setActive] = useState(0);
+  const [typePlan, setTypePlan] = useState("monthly");
+  const plans = [
+    {
+      icon: <IconArcade />,
+      type: "arcade",
+      priceMonthly: "$9/mo",
+      priceYearly: "$90/yr",
+      offerYearly: "2 months free",
+    },
+    {
+      icon: <IconAdvanced />,
+      type: "advanced",
+      priceMonthly: "$12/mo",
+      priceYearly: "$120/yr",
+      offerYearly: "2 months free",
+    },
+    {
+      icon: <IconPro />,
+      type: "pro",
+      priceMonthly: "$15/mo",
+      priceYearly: "$150/yr",
+      offerYearly: "2 months free",
+    },
+  ];
   return (
     <div className="flex flex-col gap-8 rounded-xl bg-White p-10 text-MarineBlue shadow-md lg:relative lg:gap-10 lg:rounded-none lg:bg-opacity-0 lg:p-0 lg:shadow-none">
       <div className="header-form">
@@ -13,24 +38,51 @@ export default function SelectPlan() {
       </div>
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-5 lg:flex-row">
-          <CardPlan
-            icon={<IconArcade />}
-            type="arcade"
-            price="$9/mo"
-            active={true}
-          />
-          <CardPlan icon={<IconAdvanced />} type="advanced" price="$12/mo" />
-          <CardPlan icon={<IconPro />} type="pro" price="$15/mo" />
+          {plans.map((e, index) => {
+            return (
+              <CardPlan
+                icon={e.icon}
+                type={e.type}
+                price={typePlan === "monthly" ? e.priceMonthly : e.priceYearly}
+                key={index}
+                index={index}
+                active={active}
+                setActive={setActive}
+                offer={typePlan === "yearly" ? e.offerYearly : false}
+              />
+            );
+          })}
         </div>
         <div className="flex items-center justify-center gap-8 rounded-lg bg-Alabaster p-3">
-          <span className="font-bold capitalize">monthly</span>
+          <span
+            className={`font-bold capitalize ${
+              typePlan === "monthly" ? "text-MarineBlue" : "text-CoolGray"
+            }`}
+          >
+            monthly
+          </span>
           <button
             type="button"
             className="h-[1.5rem] w-[3rem] rounded-full bg-MarineBlue px-1"
+            onClick={() => {
+              typePlan === "monthly"
+                ? setTypePlan("yearly")
+                : setTypePlan("monthly");
+            }}
           >
-            <div className="h-4 w-4 rounded-full bg-White"></div>
+            <div
+              className={`h-4 w-4 rounded-full bg-White ${
+                typePlan === "monthly" ? "float-left" : "float-right"
+              }`}
+            ></div>
           </button>
-          <span className="font-bold capitalize text-CoolGray">yearly</span>
+          <span
+            className={`font-bold capitalize ${
+              typePlan === "yearly" ? "text-MarineBlue" : "text-CoolGray"
+            }`}
+          >
+            yearly
+          </span>
         </div>
       </div>
     </div>
